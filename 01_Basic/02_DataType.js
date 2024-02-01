18e2f8ff5d66cbbf57feb193f7a3d2aceabbaa6c
milin-jadhav <122196703+milin-jadhav@users.noreply.github.com>
2024-02-01 16:02:28 +0000

    DataTypes

---
+++ 01_Basic/02_DataType.js
@@ -0,0 +1,12 @@
+let  a= 25          // number 
+let name = "John" //string
+let x = true   // boolean true /false
+                        //symbol   -- React 
+let y = null // null
+let z;      // undefined 
+
+console.log(typeof a);
+console.log(typeof name);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
